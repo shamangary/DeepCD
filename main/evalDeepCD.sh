1fb99c9584848ed684df644609e058829dae4535
Tsun-Yi Yang <shamangary@hotmail.com>
2017-08-17 19:12:44 +0800

    Add files via upload

---
+++ main/evalDeepCD.sh
@@ -0,0 +1,29 @@
+# Uncomment the command you want to operate.
+# Example: 
+#----------------------------------------------------------------------------------------------------------
+# th ./eval/fun_evalDeepCD_2S.lua "rb" 128 256 "liberty" "notredame" true 1 
+#----------------------------------------------------------------------------------------------------------
+# "rb": real-valued for leading and binary for complementary
+# 128 256: 128 dim for leading and 256 bits for complementary
+# true: DDM training is true. Otherwise its false
+# The last input is the epoch number of the model.
+
+
+#----------------------------------------------------------------------------------------------------------
+# Evaluate DeepCD 2-stream (lead: real, complementary: binary) with DDM
+th ./eval/fun_evalDeepCD_2S.lua "rb" 128 256 "liberty" "notredame" false 1 
+
+
+#----------------------------------------------------------------------------------------------------------
+# Evaluate DeepCD 2-stream (lead: real, complementary: binary) without DDM
+#th ./eval/fun_evalDeepCD_2S.lua "rb" 128 256 "liberty" "notredame" false 1 
+
+
+#----------------------------------------------------------------------------------------------------------
+# Training DeepCD 2-stream (lead: binary, complementary: binary) with DDM
+#th ./eval/fun_evalDeepCD_2S.lua "bb" 512 256 "liberty" "notredame" true 1 
+
+
+#----------------------------------------------------------------------------------------------------------
+# Training DeepCD 2-stream (lead: binary, complementary: binary) without DDM
+#th ./eval/fun_evalDeepCD_2S.lua "bb" 512 256 "liberty" "notredame" false 1 
